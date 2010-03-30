94a07292ad4b1e9e167e4078fa23ce0eec982cfe
ondrej.zara <none@none>
2010-03-30 06:01:34 +0000

    #88 toolbar hiding
    --HG--
    extra : convert_revision : svn%3Ab267cdba-c1da-11dd-874b-8bacd04a0a74/trunk%4099

--- js/wwwsqldesigner.js
+++ js/wwwsqldesigner.js
@@ -2152,6 +2152,35 @@ SQL.Options.prototype.click = function() {
 	this.dom.optionvector.checked = this.owner.getOption("vector");
 }
 
+/* ------------------ minimize/restore bar ----------- */
+
+SQL.Toggle = OZ.Class();
+
+SQL.Toggle.prototype.init = function(elm) {
+	this._state = null;
+	this._elm = elm;
+	OZ.Event.add(elm, "click", this._click.bind(this));
+	
+	var defaultState = true;
+	if (document.location.href.match(/toolbar=hidden/)) { defaultState = false; }
+	this._switch(defaultState);
+}
+
+SQL.Toggle.prototype._click = function(e) {
+	this._switch(!this._state);
+}
+
+SQL.Toggle.prototype._switch = function(state) {
+	this._state = state;
+	if (this._state) {
+		OZ.$("bar").style.height = "";
+	} else {
+		OZ.$("bar").style.overflow = "hidden";
+		OZ.$("bar").style.height = this._elm.offsetHeight + "px";
+	}
+	this._elm.className = (this._state ? "on" : "off");
+}
+
 /* --------------------- www sql designer ------------ */
 
 SQL.Designer = OZ.Class().extend(SQL.Visual);
@@ -2164,6 +2193,7 @@ SQL.Designer.prototype.init = function() {
 	this.title = document.title;
 	
 	SQL.Visual.prototype.init.apply(this);
+	new SQL.Toggle(OZ.$("toggle"));
 	
 	this.dom.container = this.dom.content = OZ.$("area");
 	this.minSize = [
