6afc321266fb926a9d05d319bfc9fc54ed82a145
bidecho <root@devshell.org>
2017-03-11 20:16:01 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 var CONFIG = {
-	AVAILABLE_DBS: ["mysql", "sqlite", "web2py", "mssql", "postgresql", "oracle", "sqlalchemy", "vfp9", "cubrid"],
+	AVAILABLE_DBS: ["mysql", "sqlite", "web2py", "mssql", "postgresql", "oracle", "sqlalchemy", "vfp9", "cubrid", "web2py"],
 	DEFAULT_DB: "mysql",
 
 	AVAILABLE_LOCALES: ["ar", "cs", "de", "el", "en", "eo", "es", "fr", "hu", "it", "ja", "nl", "pl", "pt_BR", "ro", "ru", "sv", "zh"],
 	DEFAULT_LOCALE: "en",
 	
-	AVAILABLE_BACKENDS: ["php-mysql", "php-blank", "php-file", "php-sqlite", "php-mysql+file", "php-postgresql", "php-pdo", "perl-file", "php-cubrid", "asp-file"],
+	AVAILABLE_BACKENDS: ["php-mysql", "php-blank", "php-file", "php-sqlite", "php-mysql+file", "php-postgresql", "php-pdo", "perl-file", "php-cubrid", "asp-file", "web2py"],
 	DEFAULT_BACKEND: ["php-mysql"],
 
 	RELATION_THICKNESS: 2,
