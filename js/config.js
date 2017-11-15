839550377dea29d23c608c9fcf3e1dfcbca45705
Marcelo Manzan <manzan@gmail.com>
2017-11-15 09:55:11 -0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@ var CONFIG = {
 	AVAILABLE_LOCALES: ["ar", "cs", "de", "el", "en", "eo", "es", "fr", "hu", "it", "ja", "ko", "nl", "pl", "pt_BR", "ro", "ru", "sv", "zh"],
 	DEFAULT_LOCALE: "en",
 	
-	AVAILABLE_BACKENDS: ["php-s3", "php-mysql", "php-blank", "php-file", "php-sqlite", "php-mysql+file", "php-postgresql", "php-pdo", "perl-file", "php-cubrid", "asp-file", "web2py"],
-	DEFAULT_BACKEND: ["php-s3"],
+	AVAILABLE_BACKENDS: ["php-mysql", "php-s3", "php-blank", "php-file", "php-sqlite", "php-mysql+file", "php-postgresql", "php-pdo", "perl-file", "php-cubrid", "asp-file", "web2py"],
+	DEFAULT_BACKEND: ["php-mysql"],
 
 	RELATION_THICKNESS: 2,
 	RELATION_SPACING: 15,
